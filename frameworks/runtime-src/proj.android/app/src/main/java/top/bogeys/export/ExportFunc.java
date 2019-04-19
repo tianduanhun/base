43669b8cf88cde3287d6484beaf42dd5fdf44b9c
bogey <1762076107@qq.com>
2019-04-19 17:55:53 +0800

    支持java回调lua

--- frameworks/runtime-src/proj.android/app/src/main/java/top/bogeys/export/ExportFunc.java
+++ frameworks/runtime-src/proj.android/app/src/main/java/top/bogeys/export/ExportFunc.java
@@ -1,9 +1,16 @@
 package top.bogeys.export;
 
+import org.cocos2dx.lib.Cocos2dxLuaJavaBridge;
+
 import top.bogeys.utils.UUIDUtils;
 
 
 public final class ExportFunc {
+    private static void callLuaFuncBack(final int luaFuncId, final String result){
+        Cocos2dxLuaJavaBridge.callLuaFunctionWithString(luaFuncId, result);
+        Cocos2dxLuaJavaBridge.releaseLuaFunction(luaFuncId);
+    }
+
     public static String getUUID(){
         return UUIDUtils.getUUID();
     }
